32bce9ef87854c0577acaac550c5829e44a760a5
Jared Henderson <jared@netrivet.com>
2021-05-03 11:16:06 -0400

    chapterized

---
+++ sowing-seeds-of-righteousness/updated/rename.sh
@@ -0,0 +1,10 @@
+mv 01-chapter-1.adoc 01-FOOBAR.adoc
+mv 02-chapter-2.adoc 02-FOOBAR.adoc
+mv 03-chapter-3.adoc 03-FOOBAR.adoc
+mv 04-chapter-4.adoc 04-FOOBAR.adoc
+mv 05-chapter-5.adoc 05-FOOBAR.adoc
+mv 06-chapter-6.adoc 06-FOOBAR.adoc
+mv 07-chapter-7.adoc 07-FOOBAR.adoc
+mv 08-chapter-8.adoc 08-FOOBAR.adoc
+mv 09-chapter-9.adoc 09-FOOBAR.adoc
+mv 10-chapter-10.adoc 10-FOOBAR.adoc
